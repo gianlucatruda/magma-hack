b272f033898bd910da8dddf13b0c9b1d857b894d
Thomas Tumiel <ttumiel@gmail.com>
2023-11-04 21:49:32 +0000

    dont re-embed image

--- llava/serve/gradio_web_server.py
+++ llava/serve/gradio_web_server.py
@@ -239,30 +239,24 @@ def http_bot(state, model_selector, temperature, top_p, max_new_tokens, request:
     state.messages[-1][-1] = "▌"
     yield (state, state.to_gradio_chatbot()) + (disable_btn,) * 5
 
-    try:
-        # Stream output
-        response = requests.post(worker_addr + "/worker_generate_stream",
-            headers=headers, json=pload, stream=True, timeout=10)
+    if not hasattr(self, "image_text"):
+        try:
+            # Stream output
+            response = requests.post(worker_addr + "/worker_generate_stream",
+                headers=headers, json=pload, stream=True, timeout=10)
 
-        for chunk in response.iter_lines(decode_unicode=False, delimiter=b"\0"):
-            if chunk:
-                data = json.loads(chunk.decode())
-                if data["error_code"] == 0:
-                    output = data["text"][len(prompt):].strip()
-                    state.image_text = output
-                    #yield (state, state.to_gradio_chatbot()) + (disable_btn,) * 5
-                else:
-                    output = data["text"] + f" (error_code: {data['error_code']})"
-                    state.image_text = output
-                    #yield (state, state.to_gradio_chatbot()) + (disable_btn, disable_btn, disable_btn, enable_btn, enable_btn)
-                    # return
-                # time.sleep(0.03)
+            for chunk in response.iter_lines(decode_unicode=False, delimiter=b"\0"):
+                if chunk:
+                    data = json.loads(chunk.decode())
+                    if data["error_code"] == 0:
+                        output = data["text"][len(prompt):].strip()
+                        state.image_text = output
+                    else:
+                        output = data["text"] + f" (error_code: {data['error_code']})"
+                        state.image_text = output
 
-    except requests.exceptions.RequestException as e:
-        print("RequestException", e)
-        # state.messages[-1][-1] = server_error_msg
-        #yield (state, state.to_gradio_chatbot()) + (disable_btn, disable_btn, disable_btn, enable_btn, enable_btn)
-        # return
+        except requests.exceptions.RequestException as e:
+            print("RequestException", e)
 
     # TOM, I think you want to make the Claude call here and update state before you let it yield
     state.messages[-1][-1] = "▌"
